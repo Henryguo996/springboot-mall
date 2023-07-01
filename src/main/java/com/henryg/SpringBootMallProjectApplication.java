55fe9ce35d1f071084e916dcdf5dae139ff6a8da
Henryguo996 <119644872+Henryguo996@users.noreply.github.com>
2023-07-01 14:59:24 +0800

    初始化 Spring Boot project

---
+++ src/main/java/com/henryg/SpringBootMallProjectApplication.java
@@ -0,0 +1,13 @@
+package com.henryg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMallProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMallProjectApplication.class, args);
+    }
+
+}
