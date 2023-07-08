bb62f355bdf13ebb3bd46c7a9d194b9a1f163ed4
Henryguo996 <119644872+Henryguo996@users.noreply.github.com>
2023-07-08 20:34:34 +0800

    「商品功能 - 實作查詢商品列表 (基礎實作)」

--- src/main/java/com/henryg/dao/ProductDao.java
+++ src/main/java/com/henryg/dao/ProductDao.java
@@ -3,7 +3,13 @@ package com.henryg.dao;
 import com.henryg.dto.ProductRequest;
 import com.henryg.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
+
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- src/main/java/com/henryg/service/ProductService.java
+++ src/main/java/com/henryg/service/ProductService.java
@@ -3,7 +3,13 @@ package com.henryg.service;
 import com.henryg.dto.ProductRequest;
 import com.henryg.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
+
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
