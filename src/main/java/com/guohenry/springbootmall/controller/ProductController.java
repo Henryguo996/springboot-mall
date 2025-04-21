b8591f526e3eb03d7185e4626c7cdba4fb0538ac
Henryguo996 <119644872+Henryguo996@users.noreply.github.com>
2025-04-21 22:45:25 +0800

    商品功能 - 實作查詢商品列表(分頁 + 總筆數)

--- src/main/java/com/guohenry/springbootmall/controller/ProductController.java
+++ src/main/java/com/guohenry/springbootmall/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.guohenry.springbootmall.dto.ProductQueryParams;
 import com.guohenry.springbootmall.dto.ProductRequest;
 import com.guohenry.springbootmall.model.Product;
 import com.guohenry.springbootmall.service.ProductService;
+import com.guohenry.springbootmall.util.Page;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -23,7 +24,7 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products")
-    public ResponseEntity<List<Product>> getProducts(
+    public ResponseEntity<Page<Product>> getProducts(
                                                     //查詢條件
                                                      @RequestParam(required=false) ProductCategory category,
                                                      @RequestParam(required = false) String search,
@@ -44,9 +45,18 @@ public class ProductController {
         productQueryParams.setLimit(limit);
         productQueryParams.setOffset(offset);
 
+        //取得 product list
         List<Product> productList = productService.getProducts(productQueryParams);
+        //取得 總數
+        Integer total = productService.countProduct(productQueryParams);
+        //分頁
+        Page<Product> page = new Page<>();
+        page.setLimit(limit);
+        page.setOffset(offset);
+        page.setTotal(total);
+        page.setResults(productList);
 
-           return ResponseEntity.status(HttpStatus.OK).body(productList);
+           return ResponseEntity.status(HttpStatus.OK).body(page);
     }
 
 
---
+++ src/main/java/com/guohenry/springbootmall/util/Page.java
@@ -0,0 +1,40 @@
+package com.guohenry.springbootmall.util;
+
+public class Page<T> {
+    private String limit;
+    private String offset;
+    private String total;
+    private List<T> results;
+
+    public String getLimit() {
+        return limit;
+    }
+
+    public void setLimit(String limit) {
+        this.limit = limit;
+    }
+
+    public String getOffset() {
+        return offset;
+    }
+
+    public void setOffset(String offset) {
+        this.offset = offset;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public List<T> getResults() {
+        return results;
+    }
+
+    public void setResults(List<T> results) {
+        this.results = results;
+    }
+}
